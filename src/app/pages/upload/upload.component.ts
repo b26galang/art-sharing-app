4bebdb0da6cf515548af900616d5ffd1ed1aeaa1
b26galang <galanbri@gmail.com>
2024-09-04 22:48:48 -0700

    add form for uploading image

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-upload',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    InputTextModule,
+    InputTextareaModule,
+    ButtonModule
+  ],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.css'
 })
 export class UploadComponent {
-
+  
 }

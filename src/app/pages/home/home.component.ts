f9707a0eaec4f1c4ca364cd84e1ec8e0743de42a
b26galang <galanbri@gmail.com>
2024-09-04 22:59:26 -0700

    add paginator component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ArtCardComponent } from '../../components/art-card/art-card.component';
 import { ArtCarouselComponent } from '../../components/art-carousel/art-carousel.component';
 import { CategoryTagComponent } from '../../components/category-tag/category-tag.component';
+import { PaginatorModule } from 'primeng/paginator';
+
+interface PageEvent {
+  first: number;
+  rows: number;
+  page: number;
+  pageCount: number;
+}
 
 @Component({
   selector: 'app-home',
@@ -11,11 +19,20 @@ import { CategoryTagComponent } from '../../components/category-tag/category-tag
     ArtCardComponent,
     ArtCarouselComponent,
     CategoryTagComponent,
-    CommonModule
+    CommonModule,
+    PaginatorModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  first: number = 0;
+
+  rows: number = 10;
+
+  onPageChange(event: PageEvent) {
+      this.first = event.first;
+      this.rows = event.rows;
+  }
 
 }

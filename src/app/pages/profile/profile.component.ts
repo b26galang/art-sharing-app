a42db981aae43c4152b2868c46a91c1c43437845
b26galang <galanbri@gmail.com>
2024-09-22 00:04:47 -0700

    add profile banner image

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    ImageModule
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })

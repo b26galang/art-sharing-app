0f960c15024119e09159b58ca6ff4ac9e6b35594
b26galang <galanbri@gmail.com>
2024-09-23 00:13:25 -0700

    add user info to single page view

--- src/app/components/art-card/art-card.component.ts
+++ src/app/components/art-card/art-card.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
-import { UserIconComponent } from '../user-icon/user-icon.component';
 import { ButtonModule } from 'primeng/button';
 import { ArtImageComponent } from '../art-image/art-image.component';
+import { UserIconComponent } from '../user-icon/user-icon.component';
 
 @Component({
   selector: 'app-art-card',
--- src/app/pages/art/art.component.ts
+++ src/app/pages/art/art.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { ArtImageComponent } from '../../components/art-image/art-image.component';
+import { UserIconComponent } from "../../components/user-icon/user-icon.component";
 
 @Component({
   selector: 'app-art',
   standalone: true,
-  imports: [ArtImageComponent],
+  imports: [
+    ArtImageComponent,
+    UserIconComponent
+],
   templateUrl: './art.component.html',
   styleUrl: './art.component.css'
 })

52496b68660b65a2af4fb7a78f83db3322d50a8e
b26galang <galanbri@gmail.com>
2024-12-03 20:02:59 -0800

    update card component to use data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArtCardComponent } from '../../components/art-card/art-card.component';
+import { ArtImageComponent } from '../../components/art-image/art-image.component';
 import { PaginatorComponent } from '../../components/paginator/paginator.component';
+import { PostService } from '../../services/post.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +12,24 @@ import { PaginatorComponent } from '../../components/paginator/paginator.compone
   imports: [
     ArtCardComponent,
     CommonModule,
-    PaginatorComponent
+    PaginatorComponent,
+    ArtImageComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  samplePost = {
+    title: "Artwork Title",
+    imageUrl: 'https://cdn.pixabay.com/photo/2023/12/07/11/11/girl-8435339_1280.png'
+  };
+
+  posts$: Observable<any[]> = of([]);
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.posts$ = this.postService.getPosts();
+  }
 }
